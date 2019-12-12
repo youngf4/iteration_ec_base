5aafa5fe1c2aea6d30abb67ad775a927788d98df
pengshistc <1421907693@qq.com>
2019-12-12 21:18:20 +0800

    2019年12月12日 21:18:00 教师弹出窗口---很多问题 ---sp

--- 前端代码/home/js/teacher.js
+++ 前端代码/home/js/teacher.js
@@ -1,7 +1,28 @@
+
+// 鼠标悬停在教师头像上面的控制
 $(".teacher_info").mouseover(function(){
     $(".teacher_info").css("animationPlayState","paused");
 })
 
 $(".teacher_info").mouseout(function(){
     $(".teacher_info").css("animationPlayState","running");
+})
+
+// 教师头像点击出现窗口
+$(".link_teacher").click(function(){
+    $("#light").css("display","block");
+        $(".tabbar_window").eq(0).css("background-color","#868AA7");
+        $(".information_tabpanel").css("display","block");
+        $(".research_panel").css("display","none");
+        $(".paper_panel").css("display","none");
+    $("#fade").css("display","block");
+})
+
+$(".tabbar_window").click(function (){
+    index = $(this).index();
+    $(".tabbar_window").eq(index).css("background-color","#868AA7");
+    $(".tabbar_window").eq(index).siblings().css("background-color","#63687B");
+    $(".panel_teacher_window").eq(index).css("display","block");
+    $(".panel_teacher_window").eq(index).siblings().css("display","none");  
+    $("tabbar_panel_window").css("display","block");  
 })
